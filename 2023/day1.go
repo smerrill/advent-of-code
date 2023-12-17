74d34f299bcaf0c6df0c4a0fa7e89283027fbe45
Steven Merrill <steven.merrill@gmail.com>
2023-12-17 14:15:41 -0800

    Add nicer Day 1 solution

--- 2023/day1.go
+++ 2023/day1.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"regexp"
@@ -10,6 +11,68 @@ import (
 
 func GetCalibrationsWithWords(input string) (int, error) {
 	return 0, nil
+}
+
+func ScanNumerals(data []byte, atEOF bool) (advance int, token []byte, err error) {
+	digitRegex := regexp.MustCompile(`\d`)
+	// Scan single digits
+	for i := 0; i < len(data); i += 1 {
+		r := data[i]
+		/*
+			If you want to handle multi-byte Unicode chars, do this:
+
+			for i, width := 0, 0; i < len(data); i += width {
+			var r rune
+			r, width = utf8.DecodeRune(data[i:])
+		*/
+		if digitRegex.MatchString(string(r)) {
+			return i + 1, []byte{r}, nil
+		}
+	}
+	// Request more data.
+	return 0, nil, nil
+}
+
+func ScanNumeralsAndNumberWords(data []byte, atEOF bool) (advance int, token []byte, err error) {
+	digitRegex := regexp.MustCompile(`\d`)
+	numberWords := []string{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
+	// Scan single digits
+	for i := 0; i < len(data); {
+		r := data[i]
+		if digitRegex.MatchString(string(r)) {
+			return i + 1, []byte{byte(r)}, nil
+		} else {
+			for k, v := range numberWords {
+				if string(data[i:i+len(v)]) == v {
+					// The impedance mismatch of returning a token of []byte instead of an int isn't great.
+					return i + (len(v) - 1), []byte(fmt.Sprint(k + 1)), nil
+				}
+			}
+		}
+		i += 1
+	}
+	// Request more data.
+	return 0, nil, nil
+}
+
+func GetCalibrationsScanner(input string, splitFunc bufio.SplitFunc) (int, error) {
+	sum := 0
+	for _, j := range strings.Split(input, "\n") {
+		var output []string
+		scanner := bufio.NewScanner(strings.NewReader(j))
+		scanner.Split(splitFunc)
+		for scanner.Scan() {
+			output = append(output, scanner.Text())
+		}
+		if l := len(output); l > 0 {
+			addition, err := strconv.Atoi(output[0] + output[l-1])
+			if err != nil {
+				return 0, err
+			}
+			sum += addition
+		}
+	}
+	return sum, nil
 }
 
 func GetCalibrations(input string) (int, error) {
@@ -48,9 +111,13 @@ func main() {
 	if err != nil {
 		log.Fatalf("Error reading file: %v", err)
 	}
-	sum, err := GetCalibrations(input)
+	sum1, err := GetCalibrationsScanner(input, ScanNumerals)
 	if err != nil {
 		log.Fatalf("Error getting calibrations: %v", err)
 	}
-	fmt.Println(sum)
+	sum2, err := GetCalibrationsScanner(input, ScanNumeralsAndNumberWords)
+	if err != nil {
+		log.Fatalf("Error getting calibrations: %v", err)
+	}
+	fmt.Println(sum1, sum2)
 }
--- 2023/day1_test.go
+++ 2023/day1_test.go
@@ -1,6 +1,11 @@
 package main
 
-import "testing"
+import (
+	"bufio"
+	"reflect"
+	"strings"
+	"testing"
+)
 
 const input1 = `1abc2
 pqr3stu8vwx
@@ -23,11 +28,77 @@ func TestGetCalibration(t *testing.T) {
 	if sum != 142 {
 		t.Errorf("Expected 142, got %d", sum)
 	}
-	sum, err = GetCalibrationsWithWords(input2)
+	sum, err = GetCalibrationsScanner(input1, ScanNumerals)
+	if err != nil {
+		t.Errorf("Expected nil, got %v", err)
+	}
+	if sum != 142 {
+		t.Errorf("Expected 142, got %d", sum)
+	}
+	sum, err = GetCalibrationsScanner(input2, ScanNumeralsAndNumberWords)
 	if err != nil {
 		t.Errorf("Expected nil, got %v", err)
 	}
 	if sum != 281 {
 		t.Errorf("Expected 281, got %d", sum)
 	}
+}
+
+func TestNumeralScanner(t *testing.T) {
+	testCases := map[string][]string{
+		"a1z3r5": {"1", "3", "5"},
+		"1":      {"1"},
+		"":       nil,
+		"11":     {"1", "1"},
+		"a":      nil,
+	}
+
+	for k, v := range testCases {
+		passed, output, err := testScanner(k, ScanNumerals, v)
+		if err != nil {
+			t.Errorf("Error: %v\n", err)
+		}
+		if !passed {
+			t.Errorf("Expected `%v`, got `%v`", output, v)
+		}
+	}
+}
+
+func TestNumeralAndNumberWordScanner(t *testing.T) {
+	testCases := map[string][]string{
+		"a1z3r5":      {"1", "3", "5"},
+		"1":           {"1"},
+		"":            nil,
+		"11":          {"1", "1"},
+		"a":           nil,
+		"one":         {"1"},
+		"1two3":       {"1", "2", "3"},
+		"1twotwoo3":   {"1", "2", "2", "3"},
+		"1twoone3":    {"1", "2", "1", "3"},
+		"one1twoone3": {"1", "1", "2", "1", "3"},
+		"xtwone3four": {"2", "1", "3", "4"},
+	}
+
+	for k, v := range testCases {
+		passed, output, err := testScanner(k, ScanNumeralsAndNumberWords, v)
+		if err != nil {
+			t.Errorf("Error: %v\n", err)
+		}
+		if !passed {
+			t.Errorf("Expected `%v`, got `%v`", v, output)
+		}
+	}
+}
+
+func testScanner(input string, splitFunc bufio.SplitFunc, expectedOutput []string) (bool, []string, error) {
+	var output []string
+	scanner := bufio.NewScanner(strings.NewReader(input))
+	scanner.Split(splitFunc)
+	for scanner.Scan() {
+		output = append(output, scanner.Text())
+	}
+	if scanner.Err() != nil {
+		return false, nil, scanner.Err()
+	}
+	return reflect.DeepEqual(output, expectedOutput), output, nil
 }
